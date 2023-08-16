554f77045c5e920b57d9a7439d72146cf534c87b
Son Dang <minhson0506@gmail.com>
2023-08-16 09:33:17 +0300

    initial project and database

---
+++ DataAccess/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
+
