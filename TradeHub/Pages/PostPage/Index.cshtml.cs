761c653e5aee8caafab034b934dcd3436d82b63e
Son Dang <minhson0506@gmail.com>
2023-08-17 09:00:21 +0300

    add post page template

---
+++ TradeHub/Pages/PostPage/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Models;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TradeHub.Pages.PostPage
+{
+	public class IndexModel : PageModel
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public IEnumerable<Post> ObjPostList;
+        public IndexModel(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            ObjPostList = new List<Post>();
+        }
+
+        public IActionResult OnGet()
+        {
+            ObjPostList = _unitOfWork.Post.GetAll();
+            return Page();
+        }
+    }
+}
