8be671815278250e524244d6faf53497e754681d
Son Dang <minhson0506@gmail.com>
2023-08-17 12:16:45 +0300

    change to use sendgrid

--- Utility/EmailSender.cs
+++ Utility/EmailSender.cs
@@ -27,75 +27,12 @@ namespace Utility
             emailToSend.Subject = subject;
             emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
 
-            ////send email with GMAIL ACCOUNT
-            using (var emailClient = new System.Net.Mail.SmtpClient())
-            {
-                //emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-                //emailClient.Authenticate("richfry6e");
-                //emailClient.Send(emailToSend);
-                //emailClient.Disconnect(true);
-
-
-                //emailClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                //emailClient.EnableSsl = true;
-                //emailClient.Host = "smtp.gmail.com";
-                //emailClient.Port = 587;
-                //// setup Smtp authentication
-                //System.Net.NetworkCredential credentials =
-                //    new System.Net.NetworkCredential("minhson0506@gmail.com", "MinhSonKhanhChi@123");
-                //emailClient.UseDefaultCredentials = false;
-                //emailClient.Credentials = credentials;
-
-                //MailMessage msg = new MailMessage();
-                //msg.From = new MailAddress("minhson0506@gmail.com");
-                //msg.To.Add(new MailAddress(email));
-
-                //msg.Subject = "This is a test Email subject";
-                //msg.IsBodyHtml = true;
-                //msg.Body = string.Format("<html><head></head><body><b>Test HTML Email</b></body>");
-                //emailClient.Send(msg);
-                //emailClient.Disconnect(true);
-
-                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
-                client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                client.EnableSsl = true;
-                client.Host = "smtp.gmail.com";
-                client.Port = 587;
-
-                // setup Smtp authentication
-                client.UseDefaultCredentials = false;
-                System.Net.NetworkCredential credentials =
-                    new System.Net.NetworkCredential("minhson0506@gmail.com", "MinhSonKhanhChi@123");
-                
-                client.Credentials = credentials;
-
-                MailMessage msg = new MailMessage();
-                msg.From = new MailAddress("minhson0506@gmail.com");
-                msg.To.Add(new MailAddress(email));
-
-                msg.Subject = "This is a test Email subject";
-                msg.IsBodyHtml = true;
-                msg.Body = string.Format("<html><head></head><body><b>Test HTML Email</b></body>");
-
-                try
-                {
-                    client.Send(msg);
-                   
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("fail to send mess" + ex.Message);
-                };
-            }
-
-            return Task.CompletedTask;
-
-            //var client = new SendGridClient(SendGridSecret);
-            ////THIS MUST MATCH A VERIFIED SENDGRID E-MAIL ADDRESS!!
-            //var from = new EmailAddress("minhson0506@gmail.com", "TradeHub");
-            //var to = new EmailAddress(email);
-            //var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
-            //return client.SendEmailAsync(msg);
+            var client = new SendGridClient(SendGridSecret);
+            //THIS MUST MATCH A VERIFIED SENDGRID E-MAIL ADDRESS!!
+            var from = new EmailAddress("minhson0506@gmail.com", "TradeHub");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            return client.SendEmailAsync(msg);
 
         }
     }
